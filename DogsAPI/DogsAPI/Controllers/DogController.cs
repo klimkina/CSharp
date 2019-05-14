05bb03fd8f8ba026d8483e11d95a090a55d67d1a
Liudmila Klimkina <klimkina@gmail.com>
2019-05-13 18:13:00 -0700

    Add files via upload

---
+++ DogsAPI/DogsAPI/Controllers/DogController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DogsAPI.Models;
+using DogsAPI.Services;
+
+namespace DogsAPI.Controllers
+{
+    public class DogController : ApiController
+    {
+        
+        private DogRepository dogRepository;
+        public DogController()
+        {
+            this.dogRepository = new DogRepository();
+        }
+        // GET: api/Dog
+        public Dog[] Get()
+        {
+            return dogRepository.GetAllDogs();
+        }
+
+        // GET: api/Dog/5
+        public Dog Get(int id)
+        {
+            return dogRepository.Get(id);
+        }
+
+        // POST: api/Dog
+        public HttpResponseMessage Post(Dog dog)
+        {
+            this.dogRepository.SaveDog(dog);
+
+            var response = Request.CreateResponse<Dog>(System.Net.HttpStatusCode.Created, dog);
+
+            return response;
+        }
+
+        // PUT: api/Dog/5
+        public HttpResponseMessage Put(int id, Dog updatedDog)
+        {
+            HttpResponseMessage response;
+            if(this.dogRepository.UpdateDog(id, updatedDog))
+                response = Request.CreateResponse<Dog>(System.Net.HttpStatusCode.Accepted, updatedDog);
+            else
+                response = Request.CreateResponse<Dog>(System.Net.HttpStatusCode.NotModified, updatedDog);
+            return response;
+        }
+
+        // DELETE: api/Delete/5
+        public Dog Delete(int id)
+        {
+            var dog = this.Get(id);
+            this.dogRepository.Remove(dog);
+            return dog;
+        }
+    }
+}
---
+++ DogsAPI/DogsAPI/Models/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogsAPI.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Notes { get; set; }
+    }
+}
